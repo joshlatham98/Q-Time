7eaa72b52ca6800918c9b3c8a0b81321ef770caf
Josh Latham <joshlatham@Joshs-MacBook-Pro.local>
2019-02-06 16:29:13 +0000

    Log In Storyboard
    Bit of work on sorting out the storyboards and automatic log in page

---
+++ FirebasePractice/LoginViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Firebase
+
+class LoginViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func login(_ sender: Any) {
+    }
+}
