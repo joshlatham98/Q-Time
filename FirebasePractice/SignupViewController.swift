5248472b822835fbac4b1264266a9f554d1ed4e2
Josh Latham <joshlatham@Joshs-MacBook-Pro.local>
2019-03-05 15:20:05 +0000

    user authentifacation part 1
    Sorting out the auth for user, had to edit a few functions

--- FirebasePractice/SignupViewController.swift
+++ FirebasePractice/SignupViewController.swift
@@ -3,27 +3,64 @@
 import UIKit
 import Firebase
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var nameField: UITextField!
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPasswordField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var nextBtn: UIButton!
+    
+    let picker = UIImagePickerController()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.delegate = self
 
     }
     
     @IBAction func selectImagePressed(_ sender: Any) {
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            self.imageView.image = image
+            nextBtn.isHidden = false
+        }
+        self.dismiss(animated: true, completion: nil)
+        
+        
+        
+        
     }
     
     @IBAction func nextPressed(_ sender: Any) {
+        guard nameField.text != "", emailField.text != "", passwordField.text != "", confirmPasswordField.text != "" else { return}
+        
+        if passwordField.text == confirmPasswordField.text {
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!, completion: {(user, error) in
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                
+                if let user = user 
+            })
+            
+        }else{
+            print("Password does not match")
+        }
+        
+        
+        
     }
     
     
